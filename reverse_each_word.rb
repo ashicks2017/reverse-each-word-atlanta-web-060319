f56954d6fa147ec8afcb2edd4e4cda4fbcf251e5
aeisha simmons <ashicks2017@outlook.com>
2019-05-18 22:09:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,7 @@ def reverse_each_word(string)
  orig_array = string.split(" ")
  result_array = []
  orig_array.each do |string|
- 
+ result_array << string.reverse
+end
+ result_array.join(" ")
 end
